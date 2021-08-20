437cce306c7046930edc33de72b216c5bf59f4fc
TharushiNDewangi <dewsdrv@gmail.com>
2021-08-20 23:35:50 +0530

    class.js edited

--- backend/controller/Class.js
+++ backend/controller/Class.js
@@ -1,90 +1,5 @@
 //const router = express.Router();
 const ClassSchedule = require('../models/Class');
-<<<<<<< HEAD
-const shortid = require('shortid')
-const slugify = require('slugify')
-const nodemailer = require("nodemailer");
-
-
-exports.createClass = (req, res) => {
-
-    // res.status(200).json({file: req.files , body:req.body});
-    const {
-        ClassId, hall, teachername, Studentbatch,day,starttime,endtime
-    } = req.body;
-    const classshedule = new ClassSchedule({
-        ClassId, 
-        hall,
-        teachername,
-        Studentbatch,  
-        day,
-        starttime,
-        endtime
-      
-        // createBy: req.user._id
-    });
-
-    classshedule.save(((error, ClassSchedule) => {
-        if (error){            console.log(email);
-            const receiverEmail = email; // get the reciver email address from body of the  request
-            const senderMail = "edexonlineconferencemanagement@gmail.com"; // set emailmaddress of sender
-            const password = "asdqwe@123"; // set password of sender
-    
-            try {
-                /*
-               create reusable transporter object using the default SMTP transport
-              */
-                let transporter = nodemailer.createTransport({
-                    service: "gmail", // use gmail as the email service
-                    port: 25, // port number
-                    secure: false, // true for 465, false for other ports
-                    auth: {
-                        // autnetication details
-                        user: senderMail,
-                        pass: password,
-                    },
-                    tls: {
-                        rejectUnauthorized: false,
-                    },
-                });
-        
-                let HelperOptions = {
-                    from: senderMail, // sender address
-                    to: receiverEmail, // list of receivers
-                    subject: "Your Fees received ", // Subject line
-                    text: "", // plain text body
-                    html: ` 
-                          <h3>This is an automatically generated email, please do not reply </h3>
-                          <li>Your class fees received THANKS YOU </li>
-                          <li>status: Successuly received </li>
-                          <li>amount: ${amount}</li>
-                          <li>amount: ${month}</li>
-                          <li>amount: ${year}</li>
-                          
-                          <h3>Best regards,</h3>
-                          <p>Sipni Higher Education center</p>`,
-                };
-        
-                // HTML version of the message
-        
-                transporter.sendMail(HelperOptions, (error, info) => {
-                    // send mail with defined transport object
-                    if (error) {
-                        return console.log(error);
-                    }
-        
-                    console.log("The message was sent!");
-        
-                    console.log(info);
-        
-                    res.json(info); // send the json response
-                });
-            } catch (e) {
-                console.log(e);
-            }}
-        if (ClassSchedule) {
-            res.status(201).json({ ClassSchedule });
-=======
 const Teacher = require('../models/Teacher');
 const shortid = require('shortid');
 const slugify = require('slugify');
@@ -97,7 +12,6 @@ exports.createClass = async (req, res) => {
             res.status(201).json({ id: newschedule._id });
         } catch (error) {
             res.status(406).json({ error: error.message });
->>>>>>> 3db4a0fa7ffce65c065cbec944a4aa2d07f39493
         }
     }
     //hhh
