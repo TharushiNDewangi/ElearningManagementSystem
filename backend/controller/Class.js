9363d9ba066a75a5678262452b8861860d2cda4e
shalithacode <shalithalakruwan1998@gmail.com>
2021-08-24 05:04:18 +0530

    class controller uncommented

--- backend/controller/Class.js
+++ backend/controller/Class.js
@@ -14,19 +14,7 @@ exports.createClass = async (req, res) => {
             res.status(406).json({ error: error.message });
         }
     }
-    //hhh
 };
-// exports.getclassfees=(req,res)=>
-// {
-//     Classfees.find({}).exec((error,fees)=>
-//     {
-//         if(error) return res.status(400).json({error});
-//         if(fees){
-//             const feeslist=createCatogories(fees)
-//             return res.status(201).json({feeslist});
-//         }
-//     });
-// }
 
 exports.getall = async (req, res) => {
     await ClassSchedule.find({})
@@ -39,61 +27,6 @@ exports.getall = async (req, res) => {
         });
 };
 
-// exports.getproductsbyId = (req, res) => {
-
-//     console.log('hey')
-//     //res.status(200).json({file: req.files , body:req.body});
-//     const { productid } = req.params;
-//     console.log("pro id", productid)
-//     if (productid) {
-//         Product.findOne({ _id: productid })
-//             .exec((error, product) => {
-//                 console.log('error');
-//                 if (error)
-//                     return res.status(400).json({ error });
-//                 console.log(error)
-//                 if (product) {
-//                     res.status(200).json({ product });
-//                     console.log(product);
-//                 }
-//             });
-
-//     } else {
-//         return res.status(400).json({ error: 'params required' });
-//     }
-// };
-
-// exports.deleteProduct = (req, res) => {
-//     const { productid } = req.body.payload;
-//     if (productid) {
-
-//         Product.deleteOne({ _id: productid }).exec((error, product) => {
-//             if (error)
-//                 return res.status(400).json({ error });
-//             console.log(error)
-//             if (product) {
-//                 res.status(200).json({ product });
-//                 console.log(product);
-//             }
-//         });
-
-//     } else {
-//         return res.status(400).json({ error: 'params required' });
-//     }
-// };
-// exports.editproduct = async (req, res) => {
-//     const { productid } = req.params;
-//     if (productid) {
-//         const { _id, name, price, quatity, description, offer } = req.body;
-//         const updatedproduct = {
-//             name, price, quatity, description, offer
-//         }
-//         await Product.findByIdAndUpdate(productid, updatedproduct, { new: true });
-//         res.json(updatedproduct);
-
-//     }
-
-// }
 exports.getAllTeachers = async (req, res) => {
     try {
         const getAllTeachers = await Teacher.find({}).select('name');
